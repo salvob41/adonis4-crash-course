e16e33ae0ef931910f83d9ae50a75627692525a0
salvob <tholalak@yahoo.it>
2020-01-24 18:16:47 +0100

    second lesson with Authentication

--- start/routes.js
+++ start/routes.js
@@ -24,3 +24,9 @@ Route.on('/signup').render('auth.signup');
 Route.post('/signup', 'UserController.create').validator('CreateUser');
 
 Route.on('/login').render('auth.login');
+Route.post('/login', 'UserController.login').validator('LoginUser');
+
+Route.get('/logout', async ({ auth, response }) => {
+    await auth.logout();
+    return response.redirect('/');
+});
