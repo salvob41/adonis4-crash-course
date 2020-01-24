f786a17f0bbeee49a5e864c4fcc50abea7a6949e
salvob <tholalak@yahoo.it>
2020-01-24 18:09:59 +0100

    first commit

---
+++ app/Controllers/Http/JobController.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const Job = use('App/Models/Job')
+
+class JobController {
+    async home({view}) {
+
+        // Fetch a job
+        const jobs = await Job.all();
+
+        return view.render('index', { jobs: jobs.toJSON() })
+    }
+}
+
+module.exports = JobController
---
+++ changes.md
@@ -0,0 +1,5 @@
+Following this (link)[https://coursetro.com/posts/code/170/Adonis-4-Tutorial---Learn-Adonis-4-in-this-Crash-Course]
+
+need first to do: 
+
+`npm install sqlite3 --save`
